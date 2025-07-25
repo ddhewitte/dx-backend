8527fbdac5b663f7675923a2ddd2d07a8785029c
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-07-25 23:06:07 +0700

    call API Auth Register in controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterMap } from './map/register.map';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+    @Post('register')
+    async register(@Body() data: RegisterMap) {
+        return await this.authService.register(data);
+    }
+}
