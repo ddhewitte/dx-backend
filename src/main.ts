f53a31b17f2104400cf3c6e093941d39403175a8
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-07-27 23:13:36 +0700

    Setup RabbitMQ for data stream
    - npm install @nestjs/microservices amqplib
    
    - npm install amqp-connection-manager amqplib

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+
+  //Data stream
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'karyawan_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
