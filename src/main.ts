b96911b0bc680cc702ff064f4a7bd65cdc596741
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-07-28 17:25:00 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,11 @@ async function bootstrap() {
     },
   });
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
   await app.startAllMicroservices();
 
   await app.listen(process.env.PORT ?? 3000);
